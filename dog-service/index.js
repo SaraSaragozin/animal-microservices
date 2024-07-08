5fe87dfd26e6c9cc7b0771e43e1aa30babf0da88
SaraSaragozin <saralizeth0711@gmail.com>
2024-07-07 23:34:12 -0500

    Delete unnesseary comments

--- dog-service/index.js
+++ dog-service/index.js
@@ -4,12 +4,12 @@ const axios = require('axios');
 const app = express();
 const port = 3001;
 
-const DOG_API_URL = 'https://dogapi.dog/api/v2/breeds'; // URL correcta para obtener las razas de perros
+const DOG_API_URL = 'https://dogapi.dog/api/v2/breeds';
 
 app.get('/breeds', async (req, res) => {
     try {
         const response = await axios.get(DOG_API_URL);
-        res.json(response.data.data); // Devolvemos solo el array de razas de perros
+        res.json(response.data.data);
     } catch (error) {
         console.error('Error fetching dog breeds:', error);
         res.status(500).send('Error fetching dog breeds');
