a7574fbc7fbb81f77c95c686ae5622a04a050025
SaraSaragozin <saralizeth0711@gmail.com>
2024-07-07 23:33:40 -0500

    Update DOG_API

--- dog-service/index.js
+++ dog-service/index.js
@@ -4,11 +4,14 @@ const axios = require('axios');
 const app = express();
 const port = 3001;
 
+const DOG_API_URL = 'https://dogapi.dog/api/v2/breeds'; // URL correcta para obtener las razas de perros
+
 app.get('/breeds', async (req, res) => {
     try {
-        const response = await axios.get('https://api.thedogapi.com/v2/breeds');
-        res.json(response.data);
+        const response = await axios.get(DOG_API_URL);
+        res.json(response.data.data); // Devolvemos solo el array de razas de perros
     } catch (error) {
+        console.error('Error fetching dog breeds:', error);
         res.status(500).send('Error fetching dog breeds');
     }
 });
